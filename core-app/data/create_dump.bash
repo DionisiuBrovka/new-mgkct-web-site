156be604df686ab3fe4aa96187a3ed35ed51325f
Dionisiu <dev.dionisiu.brovka@gmail.com>
2024-12-23 10:18:49 +0300

    Промежуточный фикс миграций

---
+++ core-app/data/create_dump.bash
@@ -0,0 +1,1 @@
+RUN python manage.py dumpdata --natural-foreign --indent 2 -e contenttypes -e auth.permission -e wagtailsearch.indexentry -e wagtailcore.groupcollectionpermission -e wagtailcore.grouppagepermission -e wagtailimages.rendition -e sessions > data.json
